09dc87295aa44818cc5e492de2112f2f17c5fa0e
std66 <sinkutamas@gmail.com>
2020-03-29 03:15:56 +0200

    Add empty ASP.NET Core 3.1 server
    In this commit I add an empty ASP.NET Core 3.1 server. I removed every not necessary code and assets.

---
+++ DateTimeMicroservice.Server/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DateTimeMicroservice.Server.Controllers {
+    public class HomeController : Controller {
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Index() {
+            return Ok("It works!");
+        }
+    }
+}
