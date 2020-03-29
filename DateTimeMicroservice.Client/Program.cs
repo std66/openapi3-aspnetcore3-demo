bc4c4f97f3eef4b24cecf2931dbdfa3623690782
std66 <sinkutamas@gmail.com>
2020-03-29 13:37:24 +0200

    Implement client
    In this commit I used the client generated code to perform a request to the server. Now, I have a client application that can communicate with the server.

--- DateTimeMicroservice.Client/Program.cs
+++ DateTimeMicroservice.Client/Program.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using DateTimeMicroservice.Client.OpenApiGenerated.Api;
+using DateTimeMicroservice.Client.OpenApiGenerated.Client;
+using DateTimeMicroservice.Client.OpenApiGenerated.Model;
+using System;
+using System.Threading.Tasks;
 
 namespace DateTimeMicroservice.Client {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        static async Task Main(string[] args) {
+            const string serverUrl = "http://localhost:5000";
+
+            IDateTimeApi api = new DateTimeApi(serverUrl);
+
+            try {
+                DateTimeObject response = await api.GetUtcDateTimeAsync();
+                Console.WriteLine($"Server date-time: {response.DateTime}");
+            }
+            catch (ApiException ex) {
+                Console.Error.WriteLine($"Unexpected error occurred: {ex.Message}");
+            }
         }
     }
 }
