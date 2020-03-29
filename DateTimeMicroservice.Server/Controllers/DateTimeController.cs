f331bab320a4609fb3efa3d5285f3f2140acbecf
std66 <sinkutamas@gmail.com>
2020-03-29 03:21:41 +0200

    Implement API
    In this commit I added a reference to the OpenApiGenerated project and I implemented DateTimeApiController.
    Now, I have a working API that can be invoked. So, the server is finished.

---
+++ DateTimeMicroservice.Server/Controllers/DateTimeController.cs
@@ -0,0 +1,20 @@
+﻿using DateTimeMicroservice.Server.OpenApiGenerated.Controllers;
+using DateTimeMicroservice.Server.OpenApiGenerated.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DateTimeMicroservice.Server.Controllers {
+    public class DateTimeController : DateTimeApiController {
+        public override IActionResult GetUtcDateTime() {
+            //business logic (I know, it shouldn't be here)
+            DateTime utcNow = DateTime.UtcNow;
+            
+            //presentation layer
+            return new JsonResult(
+                new DateTimeObject() {
+                    DateTime = utcNow
+                }
+            );
+        }
+    }
+}
