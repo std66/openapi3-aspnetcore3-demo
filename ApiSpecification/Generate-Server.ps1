9212068579c4304a05d33a156e7fe6ae2c0adc51
std66 <sinkutamas@gmail.com>
2020-03-29 01:44:02 +0100

    Modify server generator script to copy generated files to solution
    In this commit I modified the server generator PowerShell script to copy the generated code to the project I created in the previous commit.
    I also added some minor tweaks.

--- ApiSpecification/Generate-Server.ps1
+++ ApiSpecification/Generate-Server.ps1
@@ -3,8 +3,15 @@ $GeneratedSourceOutputDir = ".\server-generated"
 $GeneratedSourcePackageName = "DateTimeMicroservice.Server.OpenApiGenerated"
 
 function Remove-DirectoryIfExists($directory) {
-    $fullPath = Resolve-Path $directory
+    $fullPath = ""
 
+    try {
+        $fullPath = Resolve-Path $directory -ErrorAction Stop
+    }
+    catch [System.Management.Automation.ItemNotFoundException] {
+        return
+    }
+    
     if ([io.directory]::Exists($fullPath)) {
         Get-Item $fullPath | Remove-Item -Force -Recurse
     }
@@ -13,6 +20,7 @@ function Remove-DirectoryIfExists($directory) {
 #remove previous generated code if exists
 Remove-DirectoryIfExists $GeneratedSourceOutputDir
 
+#run code generator
 java -jar .\openapi-generator-cli.jar `
     generate `
     -i $OpenApiDocument `
@@ -20,3 +28,22 @@ java -jar .\openapi-generator-cli.jar `
     -g aspnetcore `
     -p generateBody=false,aspnetCoreVersion=3.0,isLibrary=true,buildTarget=library,operationModifier=abstract,useDefaultRouting=false `
     --package-name $GeneratedSourcePackageName
+
+#copy necessary files to solution
+$itemsToCopy = @(
+    'Attributes',
+    'Authentication',
+    'Controllers',
+    'Converters',
+    'Models'
+)
+
+$target = "..\$GeneratedSourcePackageName\"
+foreach ($itemToCopy in $itemsToCopy) {
+    $source = ".\$GeneratedSourceOutputDir\src\$GeneratedSourcePackageName\$itemToCopy"
+    Remove-DirectoryIfExists $($target + $itemToCopy)
+    Copy-Item -Path $source -Destination $target -Recurse
+}
+
+#remove generated code
+Remove-DirectoryIfExists $GeneratedSourceOutputDir
